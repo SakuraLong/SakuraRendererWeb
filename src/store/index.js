86aba3f98ec57ce9695fe0726a52c189e2991926
SakuraLong <105298884+SakuraLong@users.noreply.github.com>
2023-12-24 11:10:01 +0800

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,7 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
-    'githubToken': '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'
+    'githubToken': ''
   },
   getters: {
   },
